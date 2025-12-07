619488a8432de395bb5f0212b8f1a28277cfe958
Augusto Moscardo <augustomsaluz@gmail.com>
2025-12-07 14:01:20 -0300

    add mcp_server project

---
+++ mcp_server/index.ts
@@ -0,0 +1,26 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { allDefinitions as calendarDefinitions } from "./tools/calendar.js";
+import { allDefinitions as emailDefinitions } from "./tools/email.js";
+
+const allDefinitions = [...calendarDefinitions, ...emailDefinitions];
+
+const server = new McpServer({
+  name: "secretaria",
+  version: "1.0.0"
+})
+
+for (let definition of allDefinitions) {
+  server.tool(
+    definition.declaration.name,
+    definition.declaration.description,
+    definition.declaration.parameters ?? {},
+    definition.function
+  )
+}
+
+const transport = new StdioServerTransport()
+
+await server.connect(transport)
+
+console.error("MCP Server funcionando");
